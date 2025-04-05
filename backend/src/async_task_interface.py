0c489ba73ca1b95a7cc280167edebd56427f9fdb
yawn west <yawn.west@posteo.eu>
2025-04-05 11:26:27 +0200

    remove pre-commit

--- backend/src/async_task_interface.py
+++ backend/src/async_task_interface.py
@@ -13,10 +13,6 @@ class AsyncTaskInterface:
         if start:
             self.start()
 
-
-
-
-
     def start(self) -> None:
         if not self._is_running():
             self._task = asyncio.create_task(self._run())
